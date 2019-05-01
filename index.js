0155a8aa7d30125aa20637caf076b2794f44a007
Joe Ganobsik <jganobsik@gmail.com>
2019-05-01 02:14:29 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,42 @@
+let store = { drivers: [], passengers: [], trips: [] };
+
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+
+class Driver {
+  constructor(name) {
+    this.name = name;
+    this.id = driverId++;
+    store.drivers.push(this);
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.name = name;
+    this.id = passengerId++;
+    store.passengers.push(this);
+  }
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = tripId++;
+    store.trips.push(this);
+  }
+  
+    passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
+
+driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+}
